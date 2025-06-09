32a8f95976ccccb40171ccbb00b3582d5a1b087e
MrPen <lan474444@gmail.com>
2025-06-10 03:32:14 +0700

    Added prisma ORM

---
+++ prisma/migrations/20250609195944_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "StudentSex" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateEnum
+CREATE TYPE "StudentStatus" AS ENUM ('ACTIVE', 'GRADUATED', 'SUSPENDED', 'WITHDRAWN');
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "dob" TEXT NOT NULL,
+    "sex" "StudentSex" NOT NULL,
+    "faculty" TEXT NOT NULL,
+    "cohort" INTEGER NOT NULL,
+    "program" TEXT NOT NULL,
+    "address" TEXT,
+    "phone" TEXT,
+    "email" TEXT,
+    "status" "StudentStatus" NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_studentId_key" ON "Student"("studentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_phone_key" ON "Student"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
